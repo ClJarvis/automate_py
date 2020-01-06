eee78b3afa7b3dc0530acb544d80740cc9f33c3d
Chris <clj@ollure.com>
2020-01-05 22:37:28 -0600

    verifying wins

--- games/connect4.py
+++ games/connect4.py
@@ -4,14 +4,14 @@ ROW_COUNT = 6
 COLUMN_COUNT = 7
 
 def create_board():
-	board = np.zeros((6,7))
+	board = np.zeros((ROW_COUNT, COLUMN_COUNT))
 	return board
 
 def drop_piece(board, row, col, piece):
 	board[row][col] = piece 	
 
 def is_valid_location(board, col):
-	return board[5][col] == 0
+	return board[ROW_COUNT-1][col] == 0
 
 def get_next_open_row(board, col):
 	for r in range(ROW_COUNT):
@@ -20,6 +20,27 @@ def get_next_open_row(board, col):
 
 def print_board(board):
 	print(np.flip(board, 0))
+
+def winning_move(board, piece):
+	#check horizontal wins 
+	for c in range(COLUMN_COUNT-3):
+		for r in range(ROW_COUNT):
+			if board[r][c] ==piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+				return True
+
+	#check for verticle wins
+	for c in range(COLUMN_COUNT):
+		for r in range(ROW_COUNT-3):
+			if board[r][c] ==piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+				return True
+
+
+	#check for positly slope diaganola
+
+
+
+	#check for negativly slope diaganols
+
 #board = create_board()
 #print(board)
 
@@ -32,9 +53,14 @@ while not game_over:
 	#player 1 input
 	if turn == 0:
 		col = int(input("Player 1 make your selection (0-6):"))
+	
 	if is_valid_location(board, col):
 		row = get_next_open_row(board, col)
 		drop_piece(board, row, col, 1)
+
+		if winning_move(board, 1):
+			print("Player 1 wins!!")
+			game_over = True
 
 	#player 2 input
 	else:
