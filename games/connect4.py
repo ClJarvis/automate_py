64458315b537d7936c23ca108d1f6bd32671b55c
Chris <clj@ollure.com>
2020-01-06 22:42:22 -0600

    fix alternating turns, if statements werenot indented enough

--- games/connect4.py
+++ games/connect4.py
@@ -28,18 +28,24 @@ def winning_move(board, piece):
 			if board[r][c] ==piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
 				return True
 
-	#check for verticle wins
+	#check for vertical wins
 	for c in range(COLUMN_COUNT):
 		for r in range(ROW_COUNT-3):
 			if board[r][c] ==piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
 				return True
 
 
-	#check for positly slope diaganola
+	# check for positly slope diaganola
+	for c in range(COLUMN_COUNT-3):
+		for r in range(ROW_COUNT-3):
+			if board[r][c] ==piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+				return True
 
-
-
-	#check for negativly slope diaganols
+	# check for negativly slope diaganols
+	for c in range(COLUMN_COUNT-3):
+		for r in range(3, ROW_COUNT):
+			if board[r][c] ==piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+				return True
 
 #board = create_board()
 #print(board)
@@ -54,20 +60,20 @@ while not game_over:
 	if turn == 0:
 		col = int(input("Player 1 make your selection (0-6):"))
 	
-	if is_valid_location(board, col):
-		row = get_next_open_row(board, col)
-		drop_piece(board, row, col, 1)
+		if is_valid_location(board, col):
+			row = get_next_open_row(board, col)
+			drop_piece(board, row, col, 1)
 
-		if winning_move(board, 1):
-			print("Player 1 wins!!")
-			game_over = True
+			if winning_move(board, 1):
+				print("Player 1 wins!!")
+				game_over = True
 
 	#player 2 input
 	else:
 		col = int(input("Player 2 make your selection (0-6):"))
-	if is_valid_location(board, col):
-		row = get_next_open_row(board, col)
-		drop_piece(board, row, col, 2)
+		if is_valid_location(board, col):
+			row = get_next_open_row(board, col)
+			drop_piece(board, row, col, 2)
 
 	print_board(board)
 	turn += 1
