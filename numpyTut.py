4d4790ef53f5fffda8fcd6d333ebd14445b077ee
Chris <clj@ollure.com>
2020-01-16 12:14:21 -0600

    numpy tut

---
+++ numpyTut.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+a = np.array([[1,2,3,4,5,6,7],[8,9,10,11,12,13,14]])
+print(a.shape)
+
+b = np.array([[9.0,8.0,7.0],[6.0,5.0,4.0]])
+#print(b)
+
+#get dimensions
+'''print(a.ndim)
+print(b.ndim)
+
+print(a.shape)
+print(b.shape) 
+
+print(a.itemsize)
+print(b.itemsize)
+print(a.size) '''
+
